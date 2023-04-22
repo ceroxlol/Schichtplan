6cbb8b1e77f56fcf8a07962da171b05b72610585
Christopher Werner <christopher.werner1@gmx.net>
2023-04-22 13:42:42 +0200

    Make ErrorResponse Serializable so I can return it via a response.

--- src/main/kotlin/schichtplanhgl/web/ErrorExceptionMapping.kt
+++ src/main/kotlin/schichtplanhgl/web/ErrorExceptionMapping.kt
@@ -1,5 +1,8 @@
 package schichtplanhgl.web
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 internal data class ErrorResponse(val errors: Map<String, List<String?>>)
 
 object ErrorExceptionMapping {
